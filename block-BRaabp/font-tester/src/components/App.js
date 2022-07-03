ef1a5ded69dc6642bd3ca89a21b2cc117bdb7569
jiyakeshwani <jiyakeshwani4@gmail.com>
2022-07-03 15:39:02 +0530

    completed font-tester using react

---
+++ block-BRaabp/font-tester/src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Font from "./Font";
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <Font />
+      </div>
+    </>
+  );
+}
+
+export default App;
